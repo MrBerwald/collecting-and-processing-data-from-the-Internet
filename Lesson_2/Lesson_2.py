9bdb3f6c06e37ca092d3280bacf4373fa6d30dce
MrBerwald <mrberwald@gmail.com>
2022-01-04 19:18:14 +0100

    Update Lesson_2.py

--- Lesson_2/Lesson_2.py
+++ Lesson_2/Lesson_2.py
@@ -2,6 +2,7 @@ import json
 
 from bs4 import BeautifulSoup as bs
 import requests
+from pprint import pprint
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50'
@@ -9,18 +10,17 @@ headers = {
 
 main_url = 'https://hh.ru/search/vacancy'
 
-params = {'text': 'C++ junior',
-          'page': 1,
-          'salary': 'true',
-          'clusters': 'true',
-          'ored_clusters': 'true',
-          }
-
 jobs_data = []
-response = requests.get(main_url, params=params, headers=headers)
 
+page = 0
 
-while response.ok:
+while page != 5:
+    params = {'text': 'Ruby',
+              'page': page,
+              'clusters': 'true',
+              'ored_clusters': 'true',
+              }
+    response = requests.get(main_url, params=params, headers=headers)
     dom = bs(response.text, 'html.parser')
     res = dom.find('div', {'data-qa': 'vacancy-serp__results'})
     if not res:
@@ -34,6 +34,9 @@ while response.ok:
         name = src.text
         company = vacancy.find('a', {'class': 'bloko-link bloko-link_secondary'}).text
         url = src.get('href')
+        data['name'] = name
+        data['company'] = company
+        data["url"] = url
 
         try:
             salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
@@ -43,7 +46,7 @@ while response.ok:
                 data['salary_min'] = int(sr[1])
                 data['salary_max'] = 0
                 data['currency'] = sr[2]
-            elif sr[0] == 'до':
+            elif sr[0] == 'до' or '':
                 data['salary_min'] = None
                 data['salary_max'] = int(sr[1])
                 data['currency'] = sr[2]
@@ -55,16 +58,11 @@ while response.ok:
             data['salary_min'] = None
             data['salary_max'] = None
             data['currency'] = None
-        data['name'] = name
-        data['company'] = company
-        data["url"] = url
 
         jobs_data.append(data)
-    if not dom.find('a', {'data-qa': 'data-qa'}):
+    if not dom.find('a', {'class': 'bloko-button'}):
         break
-    params['page'] += 1
 
-print(jobs_data)
+    page += 1
 
-with open('result.json', 'w', encoding='utf-8') as f:
-    json.dump(jobs_data, f)
+pprint(jobs_data)
